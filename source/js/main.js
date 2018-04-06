d7339c8a448ca5c3440f359aa0c96cc1ea3e8891
Jerry20071995 <phamthingocthanh95.com>
2018-04-06 17:07:28 +0700

    edit code

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 (function($) {
 	$(function(){
-	  $('.dropdown').hover(function() {
+	  $('#menu-primary-menu .dropdown').hover(function() {
 	    $(this).find('.dropdown-menu').addClass('hover');
 	    $(this).find('.menu-item').addClass('tran');
 
@@ -10,12 +10,21 @@
 	  })
 	})
 
-	$(window).load(function() {
+	$(function(){
+	  $('.header-mobile .navbar-toggler').click(function() {
+	    $(this).addClass('open');
+
+	  }, function() {
+	    $(this).removeClass('open');
+	  })
+	})
+
 	  $('#carousel').flexslider({
 	    animation: "slide",
 	    controlNav: false,
 	    animationLoop: false,
-	    slideshow: false,
+	    slideshow: true,
+	    slideshowSpeed: 2000,
 	    itemMargin: 5,
 	    arrows: false,
 	    asNavFor: '#slider'
@@ -25,11 +34,12 @@
 	    animation: "slide",
 	    controlNav: false,
 	    animationLoop: false,
-	    slideshow: false,
+	    slideshow: true,
+	    slideshowSpeed: 2000,
 	    arrows: false,
+	    
 	    sync: "#carousel"
 	  });
-	});
 
 	$(window).load(function() {
 
